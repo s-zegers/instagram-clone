6a5cb954918b1734ee56926dfd64bb860426316a
Sebastiaan <64758560+s-zegers@users.noreply.github.com>
2020-09-04 23:22:13 +0200

    Fix more capitalization

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::middleware(['web'])->group(function () {
         Route::get('posts/create', 'PostController@create')->name('posts.create');
         Route::post('posts', 'PostController@store')->name('posts.store');
         Route::get('posts/{id}', 'PostController@show')->name('posts.show');
-        Route::delete('posts/{id}', 'Postcontroller@destroy')->name('posts.destroy');
+        Route::delete('posts/{id}', 'PostController@destroy')->name('posts.destroy');
 
         Route::get('stories/create', 'StoryController@create')->name('stories.create');
         Route::post('stories', 'StoryController@store')->name('stories.store');
